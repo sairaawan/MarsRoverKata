0f52935318a71b2fc48e6c485c1f53227e842ea2
sairaawan <syraawan@live.com>
2022-01-25 14:30:31 +0000

    Added new Testes

--- test/RoverTest.java
+++ test/RoverTest.java
@@ -1,12 +1,14 @@
 import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoverTest {
-    public MainRover rover; // declare as a private class member
+    public Rover rover; // declare as a private class member
 
     @BeforeEach
     public void setUp() throws Exception {
@@ -14,16 +16,24 @@ class RoverTest {
     }
 
     @Test
-    public void checkInstruction_L()  {
-        rover.readInstruction("MM");
-        assertEquals(rover.getY_axis(), 4);
-    }
-
-    @Test
-    public void checkInstruction_LR()  {
-        rover.readInstruction("LR");
+    public void checkInstruction_L() throws Exception {
+        rover.readInstruction("LMLMLMLMM");
+        assertEquals(rover.getX_axis(), 1);
+        assertEquals(rover.getY_axis(), 3);
         assertEquals(rover.getD(), Direction.North);
     }
 
+    @Test
+    public void checkMove() throws Exception {
+        boolean thrown = false;
+
+        try {
+            rover.readInstruction("MMMMMMMMMMM");
+        } catch (Exception e) {
+            thrown = true;
+        }
+
+        assertTrue(thrown);
+    }
 
 }
