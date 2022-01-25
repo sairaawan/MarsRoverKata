15d75512094958d7714f58784791f1f4c771dc23
sairaawan <syraawan@live.com>
2022-01-25 14:30:05 +0000

    Fixed if,else. Added checks to move function. Added constructed overloading

--- src/Rover.java
+++ src/Rover.java
@@ -1,33 +1,100 @@
 import java.util.Locale;
 
-public class Rover extends MainRover {
-    Plateau p = new Plateau(5, 5);
+public class Rover {
+    public int x_axis = 1;
+    public int y_axis = 2;
+    Plateau p = null;
+    Direction d;
 
-    public Rover(int x_axis, int y_axis, Direction direction) throws Exception {
-        super(x_axis, y_axis, direction);
+    public int getX_axis() {
+        return x_axis;
+    }
+
+    public int getY_axis() {
+        return y_axis;
+    }
+
+    public Direction getD() {
+        return d;
+    }
+
+    public Rover(Plateau p, int x_axis, int y_axis, Direction d) throws Exception {
+        this.p = p;
+        this.x_axis = x_axis;
+        this.y_axis = y_axis;
+        this.d = d;
         checkRoverPosition();
     }
 
-
-    public boolean checkRoverPosition() throws Exception {
-        if (x_axis >= p.getX_MAX() || y_axis >= p.getY_MAX())
-            throw new Exception("Rovers position is incorrect. Please enter correct coordinates");
-        return true;
+    public Rover(int x_axis, int y_axis, Direction d) throws Exception {
+        this(new Plateau(5, 5), x_axis, y_axis, d);
+        checkRoverPosition();
     }
 
-    public void readInstruction(String instruct) {
-        String i= instruct.toUpperCase();
-       String[] instructionArray = i.split("");
-       for (int ins=0; ins<instructionArray.length; ins++) {
-           System.out.println(instructionArray[ins]);
-           switch (instructionArray[ins]) {
-               case "L" -> turnLeft();
-               case "R" -> turnRight();
-               case "M" -> moveRover();
-               default -> System.out.println("Enter correct value");
-           }
+    public void moveRover() throws Exception {
+        if (d == Direction.North && this.y_axis + 1 <= this.p.getY_MAX())
+            this.y_axis += 1;
+        else if (d == Direction.East && this.x_axis + 1 <= this.p.getX_MAX())
+            this.x_axis += 1;
+        else if (d == Direction.West && this.x_axis - 1 >= 0)
+            this.x_axis -= 1;
+        else if (d == Direction.South && this.y_axis - 1 >= 0)
+            this.y_axis -= 1;
+        else
+            throw new Exception("Exceeding plateau coordinates: Movement not allowed further, Please change your direction after 5 moves");
+    }
 
+    public void turnRight() {
+        if (this.d == Direction.North) {
+            this.d = Direction.East;
+        } else if (this.d == Direction.South) {
+            this.d = Direction.West;
+
+        } else if (this.d == Direction.East) {
+            this.d = Direction.South;
+
+        } else if (this.d == Direction.West) {
+            this.d = Direction.North;
+        }
+
+    }
+
+    public void turnLeft() {
+        if (this.d == Direction.North) {
+            this.d = Direction.West;
+        } else if (this.d == Direction.South) {
+            this.d = Direction.East;
+
+        } else if (this.d == Direction.East) {
+            this.d = Direction.North;
+
+        } else if (this.d == Direction.West) {
+            this.d = Direction.South;
         }
     }
 
+    public void setPlateuMax(int x, int y) {
+        this.p = new Plateau(x, y);
+    }
+
+    public Plateau getPlateru() {
+        return this.p;
+    }
+
+    public void checkRoverPosition() throws Exception {
+        if (x_axis > p.getX_MAX() || y_axis > p.getY_MAX())
+            throw new Exception("Rovers position is incorrect. Please enter correct coordinates");
+    }
+
+    public void readInstruction(String instruct) throws Exception {
+        instruct = instruct.toUpperCase();
+        instruct = instruct.replaceAll("[^LRM]", "");
+        for (int ins = 0; ins < instruct.length(); ins++) {
+            char x = instruct.charAt(ins);
+            if (x == 'L') turnLeft();
+            if (x == 'R') turnRight();
+            if (x == 'M') moveRover();
+        }
+    }
 }
+
