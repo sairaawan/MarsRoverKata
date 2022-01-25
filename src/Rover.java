f07ec1491edf99198e0ef1a53f8bd357fe589c43
sairaawan <syraawan@live.com>
2022-01-25 21:46:21 +0000

    Revert "Fixed formatting and indentation"
    added toString method

--- src/Rover.java
+++ src/Rover.java
@@ -96,13 +96,4 @@ public class Rover {
             if (x == 'M') moveRover();
         }
     }
-
-    @Override
-    public String toString() {
-        return "Rover{" +
-                "x_axis=" + x_axis +
-                ", y_axis=" + y_axis +
-                ", d=" + d +
-                '}';
-    }
 }
