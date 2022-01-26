7208fe43f917bd6131b7649053da3b84ff38c95d
sairaawan <syraawan@live.com>
2022-01-26 07:54:27 +0000

    changed for loop with explicit objects

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,89 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
 
-        int[] plateau = new int[2];
-        int[] coordinates = new int[2];
-        Direction d = null;
-        String direction;
-
-
+    public static int getPlateauX() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter no of Rovers");
-        int r = scan.nextInt();
-
-
         System.out.println("Enter P1 coordinates");
         int p1 = scan.nextInt();
+        return p1;
+    }
+
+    public static int getPlateauY() {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Enter P2 coordinates");
         int p2 = scan.nextInt();
+        return p2;
+    }
 
-        plateau[0] = p1;
-        plateau[1] = p2;
+    public static int getCoordinatesX() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter x coordinates");
+        int x = scan.nextInt();
+        return x;
+    }
 
-        for (int i = 0; i < r; i++) {
-            System.out.println("Enter x coordinates");
-            int x = scan.nextInt();
-            System.out.println("Enter y coordinates");
-            int y = scan.nextInt();
+    public static int getCoordinatesY() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter y coordinates");
+        int y = scan.nextInt();
+        return y;
+    }
 
-            coordinates[0] = x;
-            coordinates[1] = y;
+    public static Direction getDirection() {
+        Direction d = null;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Rovers position");
+        String direction = scan.next();
+        if (direction.equalsIgnoreCase("N"))
+            return Direction.North;
+        else if (direction.equalsIgnoreCase("S"))
+            return Direction.South;
+        else if (direction.equalsIgnoreCase("E"))
+            return Direction.East;
+        else if (direction.equalsIgnoreCase("W"))
+            return Direction.West;
+        return Direction.West;
+    }
 
-            System.out.println("Enter Rovers position");
-            direction = scan.next();
-            if (direction.equalsIgnoreCase("N"))
-                d = Direction.North;
-            else if (direction.equalsIgnoreCase("S"))
-                d = Direction.South;
-            else if (direction.equalsIgnoreCase("E"))
-                d = Direction.East;
-            else if (direction.equalsIgnoreCase("W"))
-                d = Direction.West;
+    public static void main(String[] args) throws Exception {
+        int p1 = getPlateauX();
+        int p2 = getPlateauY();
 
-            Rover rover = new Rover(new Plateau(p1, p2), x, y, d);
-            System.out.println("Rover current position: " + rover);
+        System.out.println("First Rover");
 
-            System.out.println("Enter instruction for rover");
-            String instruct = scan.next();
-            rover.readInstruction(instruct);
-            System.out.println("Rover new position: " + rover);
-        }
+        int x = getCoordinatesX();
+        int y = getCoordinatesY();
+        Direction d = getDirection();
+
+
+        Rover rover = new Rover(new Plateau(p1, p2), x, y, d);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter instruction for rover");
+        String instruct = scan.next();
+
+        System.out.println("First Rover starting position: " + rover);
+        rover.readInstruction(instruct);
+        System.out.println("First Rover new position: " + rover);
+
+        x = getCoordinatesX();
+        y = getCoordinatesY();
+        d = getDirection();
+
+        Rover rover2 = new Rover(new Plateau(p1, p2), x, y, d);
+        System.out.println("Rover current position: " + rover2);
+
+        System.out.println("Second Rover");
+
+        System.out.println("Enter instruction for second rover");
+        instruct = scan.next();
+        rover2.readInstruction(instruct);
+        System.out.println("Rover new position: " + rover2);
+
+        System.out.println("See you next time. Bye");
 
     }
+
 
 }
