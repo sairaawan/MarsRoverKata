571f12b516dbb43df307e16f833c25dde9560707
sairaawan <syraawan@live.com>
2022-01-26 08:08:33 +0000

    Continue moving rover with single instruction using do while

--- src/Main.java
+++ src/Main.java
@@ -52,42 +52,36 @@ public class Main {
         int p2 = getPlateauY();
 
         System.out.println("First Rover");
-
         int x = getCoordinatesX();
         int y = getCoordinatesY();
         Direction d = getDirection();
-
-
         Rover rover = new Rover(new Plateau(p1, p2), x, y, d);
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter instruction for rover");
-        String instruct = scan.next();
-
         System.out.println("First Rover starting position: " + rover);
-        rover.readInstruction(instruct);
-        System.out.println("First Rover new position: " + rover);
 
+        System.out.println("Second Rover");
         x = getCoordinatesX();
         y = getCoordinatesY();
         d = getDirection();
-
         Rover rover2 = new Rover(new Plateau(p1, p2), x, y, d);
-        System.out.println("Rover current position: " + rover2);
+        System.out.println("Second Rover current position: " + rover2);
 
-        System.out.println("Second Rover");
-
-        System.out.println("Enter instruction for second rover");
-        instruct = scan.next();
-        rover2.readInstruction(instruct);
-        System.out.println("Rover new position: " + rover2);
-
-        if (rover.x_axis == rover2.x_axis && rover.y_axis == rover2.y_axis) {
-            throw new Exception("Warning! Rovers are at same position");
-        }
+        String response;
+        do {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter instruction for rovers");
+            String instruct = scan.next();
+            rover.readInstruction(instruct);
+            System.out.println("First Rover new position: " + rover);
+            rover2.readInstruction(instruct);
+            System.out.println("Rover new position: " + rover2);
+            if (rover.x_axis == rover2.x_axis && rover.y_axis == rover2.y_axis) {
+                throw new Exception("Warning! Rovers are at same position. Collision expected");
+            }
+            System.out.println("Continue moving Rovers ? Enter y/n");
+            response = scan.next();
+        } while (response.equalsIgnoreCase("n"));
 
         System.out.println("See you next time. Bye");
-
-
     }
 
 
