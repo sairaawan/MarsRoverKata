cd14694d5e7569d7127c14f764b0199f4908ef36
sairaawan <syraawan@live.com>
2022-02-05 13:58:33 +0000

    removed extra setters

--- src/Rover.java
+++ src/Rover.java
@@ -10,17 +10,11 @@ public class Rover implements VehicleRover {
         return x_axis;
     }
 
-    public void setX_axis(int x_axis) {
-        this.x_axis = x_axis;
-    }
 
     public int getY_axis() {
         return y_axis;
     }
 
-    public void setY_axis(int y_axis) {
-        this.y_axis = y_axis;
-    }
 
     public Direction getD() {
         return direction;
