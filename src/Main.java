c01c6830cbd295d7bfed51fc01c51771a8469369
sairaawan <syraawan@live.com>
2022-02-06 07:04:05 +0000

    using vehicle instead of rover to read instructions. Adding comments

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class Main {
     private static Scanner scan = new Scanner(System.in);
 
+
     public static int getPlateauX() {
         System.out.println("Enter P1 coordinates");
         int p1 = scan.nextInt();
         return p1;
     }
+
 
     public static int getPlateauY() {
         System.out.println("Enter P2 coordinates");
@@ -44,28 +46,40 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
+
+//        First Rover creation
+
         int p1 = getPlateauX();
         int p2 = getPlateauY();
         System.out.println("First Rover");
+
         int x = getCoordinatesX();
         int y = getCoordinatesY();
         Direction d = getDirection();
         Rover rover = new Rover(new Plateau(p1, p2), x, y, d);
-        System.out.println("First Rover starting position: " + rover);
+        Vehicle vehicle = new Vehicle(rover);
+        System.out.println("First Rover starting position: " + vehicle);
+
+//        Second Rover creation
+
         System.out.println("Second Rover");
         x = getCoordinatesX();
         y = getCoordinatesY();
         d = getDirection();
         Rover rover2 = new Rover(new Plateau(p1, p2), x, y, d);
-        System.out.println("Second Rover current position: " + rover2);
+        Vehicle vehicle2 = new Vehicle(rover2);
+        System.out.println("Second Rover current position: " + vehicle2);
+
+//        Giving instructions to both rovers
+
         String response;
         do {
 
             System.out.println("Enter instruction for rovers");
             String instruct = scan.next();
-            rover.readInstruction(instruct);
+            vehicle.readInstruction(instruct);
             System.out.println("First Rover new position: " + rover);
-            rover2.readInstruction(instruct);
+            vehicle2.readInstruction(instruct);
             System.out.println("Second Rover new position: " + rover2);
             if (rover.getX_axis() == rover2.getX_axis() && rover.getY_axis() == rover2.getY_axis()) {
                 throw new Exception("Warning! Rovers are at same position. Collision expected");
